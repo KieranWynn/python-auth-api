f08576cbe1823c5df5986709801f0cc58bb83c84
Kieran Wynn <kieranwynn+git@gmail.com>
2021-05-24 23:04:56 +1000

    add_post route

--- api/api.py
+++ api/api.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from typing import Any, Dict
 
 from .dummy_data import posts, users
+from api.model import PostSchema
 
 app = FastAPI()
 
@@ -20,5 +21,12 @@ async def get_one_post(id: int) -> Dict[str, Any]:
         if post.get("id") == id:
             return {"data": post}
     else:
-        return {"error": f"No such post with ID={id}."}
+        return {"error": f"No such post with Id={id}."}
 
+@app.post("/posts", tags=["posts"])
+async def add_post(post: PostSchema) -> Dict[str, Any]:
+    post.id = len(posts) + 1
+    posts.append(post.dict())
+    return {
+        "data": "post successfully added"
+    }
